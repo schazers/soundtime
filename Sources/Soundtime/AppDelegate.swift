6daabc0cbc2a3e25d711dca06b1cc31bb129b50c
schazers <schazers@gmail.com>
2026-06-03 00:17:55 -0700

    Add macOS app menus

--- Sources/Soundtime/AppDelegate.swift
+++ Sources/Soundtime/AppDelegate.swift
@@ -1,9 +1,12 @@
 import AppKit
 
+@MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var mainWindowController: MainWindowController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        configureMainMenu()
+
         let controller = MainWindowController()
         mainWindowController = controller
         controller.showWindow(nil)
@@ -12,5 +15,51 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
+    }
+
+    private func configureMainMenu() {
+        let mainMenu = NSMenu()
+
+        let appMenuItem = NSMenuItem(title: "Soundtime", action: nil, keyEquivalent: "")
+        mainMenu.addItem(appMenuItem)
+        appMenuItem.submenu = makeApplicationMenu()
+
+        let editMenuItem = NSMenuItem(title: "Edit", action: nil, keyEquivalent: "")
+        mainMenu.addItem(editMenuItem)
+        editMenuItem.submenu = makeEditMenu()
+
+        NSApplication.shared.mainMenu = mainMenu
+    }
+
+    private func makeApplicationMenu() -> NSMenu {
+        let menu = NSMenu(title: "Soundtime")
+
+        menu.addItem(NSMenuItem(
+            title: "Export...",
+            action: #selector(TimelineView.exportAudio(_:)),
+            keyEquivalent: "e"
+        ))
+        menu.addItem(.separator())
+        let quitItem = NSMenuItem(
+            title: "Quit Soundtime",
+            action: #selector(NSApplication.terminate(_:)),
+            keyEquivalent: "q"
+        )
+        quitItem.target = NSApplication.shared
+        menu.addItem(quitItem)
+
+        return menu
+    }
+
+    private func makeEditMenu() -> NSMenu {
+        let menu = NSMenu(title: "Edit")
+
+        menu.addItem(NSMenuItem(
+            title: "Undo",
+            action: #selector(TimelineView.undoTimelineEdit(_:)),
+            keyEquivalent: "z"
+        ))
+
+        return menu
     }
 }
