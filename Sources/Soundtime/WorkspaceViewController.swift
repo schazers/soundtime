ca5cae8ac7dd3b74b0abf065ca3ac0d649164c5b
schazers <schazers@gmail.com>
2026-06-05 15:54:29 -0700

    Add new project windows

--- Sources/Soundtime/WorkspaceViewController.swift
+++ Sources/Soundtime/WorkspaceViewController.swift
@@ -1,12 +1,26 @@
 import AppKit
 
 final class WorkspaceViewController: NSViewController {
+    private let restoresLastProject: Bool
+
+    init(restoresLastProject: Bool = true) {
+        self.restoresLastProject = restoresLastProject
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        restoresLastProject = true
+        super.init(coder: coder)
+    }
+
     override func loadView() {
         view = WorkspaceView()
     }
 
     override func viewDidAppear() {
         super.viewDidAppear()
-        (view as? WorkspaceView)?.restoreLastProjectIfNeeded()
+        if restoresLastProject {
+            (view as? WorkspaceView)?.restoreLastProjectIfNeeded()
+        }
     }
 }
